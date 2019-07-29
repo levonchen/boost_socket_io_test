a52274e74fa7242305ebf7685d43e94438cad83f
lfchen <levon.chen@foxmail.com>
2019-07-29 22:48:54 +0800

    main update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
-#include "iostream"
-using namespace std;
-#define BOOST_ALL_DYN_LINK
-#include "boost/thread.hpp"
+// #include "iostream"
+// using namespace std;
+// #define BOOST_ALL_DYN_LINK
+// #include "boost/thread.hpp"
 
-void threadFunc()
-{
-	cout<< "thread hello world!" <<endl;
-}
-int main()
-{
-	boost::function<void()> tdfunc(threadFunc);
-	boost::thread td(tdfunc);
-	td.join();
+// void threadFunc()
+// {
+	// cout<< "thread hello world!" <<endl;
+// }
+// int main()
+// {
+	// boost::function<void()> tdfunc(threadFunc);
+	// boost::thread td(tdfunc);
+	// td.join();
 	
-	return 0;
+	// return 0;
+// }
+
+
+
+#include<iostream>
+#include<boost/bind.hpp>
+using namespace std;
+using namespace boost;
+
+int fun(int x,int y){return x+y;}
+int main(){
+    int m=1;int n=2;
+    cout<<boost::bind(fun,_1,_2)(m,n)<<endl;
+    return 0;
 }
