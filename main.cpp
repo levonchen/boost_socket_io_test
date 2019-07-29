80498e13abe1841674829b31b2fda7ec5d0f0c73
lfchen <levon.chen@foxmail.com>
2019-07-29 21:57:37 +0800

    update code

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 #define BOOST_ALL_DYN_LINK
 #include "boost/thread.hpp"
+
 void threadFunc()
 {
-    cout<<"thread hello world!"<<endl;
+	cout<< "thread hello world!" <<endl;
 }
-void main()
+int main()
 {
-boost::function<void()> tdfunc(threadFunc);
-boost::thread td(tdfunc);
-td.join();
+	boost::function<void()> tdfunc(threadFunc);
+	boost::thread td(tdfunc);
+	td.join();
+	
+	return 0;
 }
