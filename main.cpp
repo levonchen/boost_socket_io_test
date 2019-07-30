803dbbd0389bddd2e02fc8c4e5d5e4e15e22d085
lfchen <levon.chen@foxmail.com>
2019-07-30 13:23:14 +0800

    test asio

--- main.cpp
+++ main.cpp
@@ -20,12 +20,39 @@
 
 #include<iostream>
 #include<boost/bind.hpp>
+#include<boost/asio.hpp>
+
 using namespace std;
 using namespace boost;
+using namespace boost::asio;
 
 int fun(int x,int y){return x+y;}
-int main(){
-    int m=1;int n=2;
+
+void basic_test()
+{
+	 int m=1;int n=2;
     cout<<boost::bind(fun,_1,_2)(m,n)<<endl;
+}
+
+
+void handler(const boost::system::error_code &ec)
+{
+	cout<<"1 S." <<std::endl;
+}
+
+
+void asio_test()
+{
+	boost::asio::io_service io_service;
+	boost::asio::deadline_timer time(io_service,boost::posic_time::seconds(1));
+	
+	time.async_wait(handler);
+	
+	io_service.run();
+	cout<<"Hello world io_service run end"<<std::endl;
+}
+
+int main(){
+   
     return 0;
 }
